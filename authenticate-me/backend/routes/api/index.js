e7e1dad687d9b84ac34474e3fc0ed2fb14666389
Huydu Tran <duutran87@icloud.com>
2022-05-10 21:49:09 -0700

    finished testing.  Start phase 4

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -6,28 +6,4 @@ router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
 
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      }
-    });
-  setTokenCookie(res, user);
-  return res.json({ user });
-});
-
-router.get('/restore-user',
-  restoreUser,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
-router.get('/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-
 module.exports = router;
