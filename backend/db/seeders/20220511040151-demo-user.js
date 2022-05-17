0298342c157af52f527427673526beead3d28ddd
Huydu Tran <duutran87@icloud.com>
2022-05-17 15:53:43 -0700

    fixed typo 'userName' in seeder

--- backend/db/seeders/20220511040151-demo-user.js
+++ backend/db/seeders/20220511040151-demo-user.js
@@ -8,21 +8,21 @@ module.exports = {
         email: 'demo@user.io',
         firstName: 'Demo',
         lastName: 'Lition',
-        username: 'Demo-lition',
+        userName: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         firstName: 'Fake',
         lastName: 'User',
-        username: 'FakeUser1',
+        userName: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         firstName: 'Fake',
         lastName: 'User2',
-        username: 'FakeUser2',
+        userName: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], {});
