82f3d2dc871a60f2b0dba9fc1afa5131cf37e5ba
Huydu Tran <duutran87@icloud.com>
2022-05-17 15:20:48 -0700

    added seeder for comments

---
+++ backend/db/seeders/20220517221428-demo-comment.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Comments', [
+      {
+        userId: 1,
+        songId: 1,
+        body: "What a wonderful song"
+      },
+      {
+        userId: 2,
+        songId: 2,
+        body: "Awesome hip hop song"
+      },
+      {
+        userId: 3,
+        songId: 3,
+        body: "This song is poppin"
+      },
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Comments', {
+      userId: { [Op.in]: [1, 2, 3]}
+    }, {})
+  }
+};
