b3dc8f69047be4c79f3049817ca77df89300c000
Huydu Tran <duutran87@icloud.com>
2022-05-17 13:04:13 -0700

    added model/migration for comments

---
+++ backend/db/migrations/20220517195933-create-comment.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      songId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Comments');
+  }
+};
---
+++ backend/db/models/comment.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Comment.init({
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    songId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    body: {
+      allowNull: false,
+      type: DataTypes.STRING
+    }
+  }, {
+    sequelize,
+    modelName: 'Comment',
+  });
+  return Comment;
+};
