2d1223c14e33a88f6c63d2deec53dc71e2b81202
Huydu Tran <duutran87@icloud.com>
2022-05-17 13:19:20 -0700

    finished all table validations/constraints

---
+++ backend/db/migrations/20220517201322-create-playlist-song.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('PlaylistSongs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      playlistId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      songId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('PlaylistSongs');
+  }
+};
---
+++ backend/db/models/playlistsong.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class PlaylistSong extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  PlaylistSong.init({
+    playlistId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    songId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    }
+  }, {
+    sequelize,
+    modelName: 'PlaylistSong',
+  });
+  return PlaylistSong;
+};
