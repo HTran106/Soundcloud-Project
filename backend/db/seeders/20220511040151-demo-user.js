cf670d8b88b8666cf69025def84f5d67ff13f263
Huydu Tran <duutran87@icloud.com>
2022-05-17 12:26:11 -0700

    created dev.db

--- backend/db/seeders/20220511040151-demo-user.js
+++ backend/db/seeders/20220511040151-demo-user.js
@@ -6,16 +6,22 @@ module.exports = {
     return queryInterface.bulkInsert('Users', [
       {
         email: 'demo@user.io',
+        firstName: 'Demo',
+        lastName: 'Lition',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
+        firstName: 'Fake',
+        lastName: 'User',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
+        firstName: 'Fake',
+        lastName: 'User2',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
       }
