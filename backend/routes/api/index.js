279ef0e960f02038ed801c98a1db562ae455ee87
Huydu Tran <duutran87@icloud.com>
2022-05-18 12:43:22 -0700

    added some test routes in api/session

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,8 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.use('/session', sessionRouter);
 
@@ -15,5 +16,24 @@ router.get('/', async (req, res) => {
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
+
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
+
+const { restoreUser } = require('../../utils/auth.js');
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 module.exports = router;
--- backend/routes/my/users.js
+++ backend/routes/my/users.js
@@ -5,8 +5,24 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const { jwtConfig } = require('../../config');
+const user = require('../../db/models/user');
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 
 
-router.get('/info', )
+
+router.get('/info', async (req, res, next) => {
+    const user = await User.scope('currentUser').findByPk(user.id)
+    console.log(user)
+})
 
 module.exports = router
