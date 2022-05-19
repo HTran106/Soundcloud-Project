d9a89803bfd7d62e5ac0a7b21b15420ffb93e2b8
Huydu Tran <duutran87@icloud.com>
2022-05-19 11:52:49 -0700

    added route for Get all albums of an artist based on ID

--- backend/routes/artists/artists.js
+++ backend/routes/artists/artists.js
@@ -6,6 +6,50 @@ const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth')
 const { User, Song, Album, sequelize } = require('../../db/models');
 const { jwtConfig } = require('../../config');
 
+
+
+
+
+//Get all albums of an artist based on the artist ID
+router.get('/:userId/albums', async (req, res, next) => {
+    const { userId } = req.params;
+
+    const artist = await User.findByPk(userId)
+
+    if (artist) {
+        const albums = await Album.findAll({where: {userId,}})
+        res.json(albums)
+    } else {
+        const err = new Error('Artist does not exist')
+        err.title = 'Artist does not exist'
+        err.status = 404;
+        return next(err)
+    }
+})
+
+//Get all Songs of an artist based on ID
+router.get('/:userId/songs', async (req, res, next) => {
+    const { userId } = req.params;
+
+
+    const artist = await User.findByPk(userId)
+
+
+    if (artist) {
+        const allSongs = await Song.findAll({
+            where: {userId,}
+        })
+
+        res.json(allSongs)
+    } else {
+        const err = new Error('Artist does not exist');
+        err.title = 'Artist does not exist';
+        err.status = 404;
+        return next(err)
+    }
+
+})
+
 //get details of an Artist from ID
 router.get('/:userId', async (req, res, next) => {
     const { userId } = req.params;
@@ -14,7 +58,7 @@ router.get('/:userId', async (req, res, next) => {
     const totalAlbums = await Album.count()
 
     const artist = await User.findByPk(userId, {
-        attributes: ['id', 'username', ], //still need to add previewImage
+        attributes: ['id', 'username', ], //still need to add previewImage //////have to merge other 2 routes to dev first
     })
 
     if (artist) {
