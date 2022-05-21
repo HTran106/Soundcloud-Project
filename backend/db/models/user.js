ce2e7f0aa9e6c5ea7ac72b113f6da72f3d504c1c
Huydu Tran <duutran87@icloud.com>
2022-05-21 09:28:24 -0700

    added previewImage to users table

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -110,7 +110,8 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [60, 60]
         }
-      }
+      },
+      previewImage: DataTypes.STRING
   }, {
     defaultScope: {
       attributes: {
