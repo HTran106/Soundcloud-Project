498a6c3b29d2c0cb169e27532d3abaf147cf1c5f
Huydu Tran <duutran87@icloud.com>
2022-05-23 15:57:15 -0700

    fixed error messages from an array to an object to match readme

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -7,15 +7,18 @@ const { check, validationResult } = require('express-validator');
 // (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, _res, next) => {
   const validationErrors = validationResult(req);
-
+  let errorObj = {}
   if (!validationErrors.isEmpty()) {
     const errors = validationErrors
     .array()
-    .map((error) => `${error.msg}`);
+    // .map((error) => `${error.msg}`);
+    .forEach((error) => {
+      errorObj[error.param] = error.msg
+    })
 
 
     const err = Error('Validation error');
-    err.errors = errors;
+    err.errors = errorObj;
     err.status = 400;
     err.title = 'Validation error';
     next(err);
