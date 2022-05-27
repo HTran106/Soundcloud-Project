49209ddcd9b4f170dd4c23c1e1c2cd4df223aa09
Huydu Tran <duutran87@icloud.com>
2022-05-26 17:33:58 -0700

    added pagination to users route

--- backend/routes/my/users.js
+++ backend/routes/my/users.js
@@ -10,26 +10,56 @@ const { pagination, validatePagination } = require('../../utils/validation');
 //Get all Albums created by the Current User
 router.get('/albums', requireAuth, restoreUser, async (req, res) => {
   const { user } = req;
+  let { page, size } = req.query;
+
+  if (!size) size = 20
+  if (!page) page = 0
+
+  page = parseInt(page);
+  size = parseInt(size);
+
+  page > 10 ? page = 0 : page = page
+  size > 20 ? size = 20 : size = size
 
   const albums = await Album.findAll({
     where: {
-      userId: user.id
+      userId: user.id,
+      ...pagination(page, size)
     }
   })
 
-  res.json({Albums: albums})
+  res.json({
+    Albums: albums,
+    page,
+    size,
+  })
 })
 
 //Get all Songs created by current User
 router.get('/songs', requireAuth, restoreUser, async (req, res) => {
   const { user } = req;
+  let { page, size } = req.query;
+
+  if (!size) size = 20
+  if (!page) page = 0
+
+  page = parseInt(page);
+  size = parseInt(size);
+
+  page > 10 ? page = 0 : page = page
+  size > 20 ? size = 20 : size = size
 
   const songs = await Song.findAll({
     where: {
-      userId: user.id
+      userId: user.id,
+      ...pagination(page,size)
     }
   })
-  res.json({Songs: songs})
+  res.json({
+    Songs: songs,
+    page,
+    size,
+  })
 })
 
 //get all info on current user
