f54d2826d57f0111d05c6fb1ab12097d9b550a80
Huydu Tran <duutran87@icloud.com>
2022-05-26 20:39:52 -0700

    changed error formatter to not show stack in production

--- backend/app.js
+++ backend/app.js
@@ -73,12 +73,14 @@ app.use((err, _req, _res, next) => {
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
+  const options = {}
+  if (!isProduction) options.stack = err.stack
   res.json({
     // title: err.title || 'Server Error',
     statusCode: res.statusCode,
     message: err.message,
     errors: err.errors,
-    stack: isProduction ? null : err.stack
+    ...options
   });
 });
 
