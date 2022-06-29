77b87fbc08818e7d28b2011d1e5e050c1c80eb1b
Huydu Tran <duutran87@icloud.com>
2022-06-29 10:17:32 -0700

    added songs component

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -9,7 +9,7 @@ const SearchBarComponent = () => {
                 placeholder="Search for artists, bands, tracks, podcasts"
             />
             <button type='submit' className='search-button'>
-                <img src='https://www.kindacode.com/wp-content/uploads/2020/12/search.png'></img>
+                <img src='https://www.kindacode.com/wp-content/uploads/2020/12/search.png' alt='mag-glass'></img>
             </button>
             <p id="search-p">or</p>
             <button className='upload-own-button'>Upload your own</button>
---
+++ frontend/src/components/SongsComponent/index.js
@@ -0,0 +1,7 @@
+const SongsComponent = () => {
+    return (
+
+    )
+}
+
+export default SongsComponent
