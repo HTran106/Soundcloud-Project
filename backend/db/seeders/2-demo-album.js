5faf9e2d3325ca3dd3e258bb9c189850dfdc2b08
Huydu Tran <duutran87@icloud.com>
2022-07-01 09:28:33 -0700

    finished seeders

--- backend/db/seeders/2-demo-album.js
+++ backend/db/seeders/2-demo-album.js
@@ -6,47 +6,70 @@ module.exports = {
       {
         userId: 1,
         title: 'Happy',
-        description: 'Wonderful world album',
-        previewImage: 'www.albumPreviewImageTEST.com'
+        description: 'Compilation of all songs that make you happy.',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/album1.jpeg'
       },
       {
         userId: 2,
         title: 'Hopeful',
-        description: 'Top 10 Hip-hop Songs',
-        previewImage: 'www.albumPreviewImageTEST.com'
+        description: 'Compilation of all songs that make you hopeful.',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/album2.jpeg'
       },
       {
         userId: 3,
         title: 'Epic',
-        description: 'Best Pop music',
-        previewImage: 'www.albumPreviewImageTEST.com'
+        description: 'Compilation of the most epic songs.',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/album3.jpeg'
+      },
+      {
+        userId: 4,
+        title: 'Dreamy',
+        description: 'Dreamy music for people that likes to dream.',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/album4.jpeg'
+      },
+      {
+        userId: 5,
+        title: 'Laid Back',
+        description: 'Songs that make you feel relaxed and laid back.',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/album5.jpeg'
       },
       {
         userId: 1,
-        title: 'Album 4',
-        description: 'Best Pop music',
-        previewImage: 'www.albumPreviewImageTEST.com'
+        title: 'Peaceful',
+        description: 'Most peaceful songs on Earth.',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/album6.jpeg'
       },
       {
         userId: 2,
-        title: 'Album 5',
-        description: 'Best Pop music',
-        previewImage: 'www.albumPreviewImageTEST.com'
+        title: 'Lovely',
+        description: 'Music made with love.',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/album7.jpeg'
       },
       {
-        userId: 2,
-        title: 'Album 6',
-        description: 'Best Pop music',
-        previewImage: 'www.albumPreviewImageTEST.com'
+        userId: 3,
+        title: 'Glamorous',
+        description: 'This album will make you feel like the Kardashians',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/album8.jpeg'
       },
-
+      {
+        userId: 4,
+        title: 'Smooth',
+        description: 'Smooth like butter.',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/album9.jpeg'
+      },
+      {
+        userId: 5,
+        title: 'Sentimental',
+        description: 'Sentimental music for everyone.',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/album10.jpeg'
+      },
     ], {});
   },
 
   async down (queryInterface, Sequelize) {
     const Op = Sequelize.Op;
     await queryInterface.bulkDelete('Albums', {
-      title: { [Op.in]: ['Wonderland', 'Hip-hop Album', 'Pop Album', 'Album 4', 'Album 5', 'Album 6']}
+      id: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
     })
   }
 };
--- backend/db/seeders/3-demo-song.js
+++ backend/db/seeders/3-demo-song.js
@@ -23,201 +23,153 @@ module.exports = {
         albumId: 3,
         userId: 3,
         title: 'As Lovely as You',
-        description: "Cute pop song",
+        description: "Lovely",
         url: "https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_As+Lovely+as+You+-+Wanderer's+Trove.mp3",
         previewImage: "https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_As+Lovely+as+You+BIG-+Wanderer's+Trove.jpeg"
       },
       {
-        albumId: 2,
-        userId: 1,
+        albumId: 4,
+        userId: 4,
         title: 'Body Bounce',
         description: "Hopeful, Laid Back",
         url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Body+Bounce+-+Ballpoint.mp3',
         previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Body+Bounce+BIG+-+Ballpoint.jpeg'
       },
       {
-        albumId: 2,
-        userId: 1,
+        albumId: 5,
+        userId: 5,
         title: 'Champagne Toast',
         description: "Happy, Laid Back",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Champagne+Toast+-+Nbhd+Nick.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Champagne+Toast+BIG+-+Nbhd+Nick.jpeg'
       },
       {
-        albumId: 2,
+        albumId: 1,
         userId: 1,
         title: 'Cold Blooded',
         description: "Hopeful, Busy & Frantic",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_cold+blooded+-+dasloe.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_cold+blooded+BIG-+dasloe.jpeg'
       },
       {
         albumId: 2,
         userId: 2,
         title: 'Crazy like That',
         description: "Dreamy, Laid Back",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Crazy+like+That+-+Lofive.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Crazy+like+That+BIG-+Lofive.jpeg'
       },
       {
-        albumId: 2,
-        userId: 2,
+        albumId: 3,
+        userId: 3,
         title: 'Eu Quero Ver o Oceano',
         description: "Smooth, Glamorous",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Eu+Quero+Ver+o+Oceano+BIG-+John+Runefelt.jpeg',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Eu+Quero+Ver+o+Oceano+BIG-+John+Runefelt.jpeg'
       },
       {
-        albumId: 2,
-        userId: 2,
+        albumId: 4,
+        userId: 4,
         title: 'For What Is Right',
         description: "Epic, Dreamy",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_For+What+Is+Right+-+Trevor+Kowalski.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_For+What+Is+Right+BIG+-+Trevor+Kowalski.jpeg'
       },
       {
-        albumId: 2,
-        userId: 2,
+        albumId: 5,
+        userId: 5,
         title: 'Future Is Now',
         description: "Epic, Hopeful",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Future+Is+Now+-+FLYIN.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Future+Is+Now+BIG-+FLYIN.jpeg'
       },
       {
-        albumId: 2,
-        userId: 2,
+        albumId: 1,
+        userId: 1,
         title: 'Get Out of My Way',
         description: "Happy, Hopeful",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Get+Out+of+My+Way+-+Tape+Machines.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Get+Out+of+My+Way+BIG+-+Tape+Machines.jpeg'
       },
       {
         albumId: 2,
         userId: 2,
         title: 'Havana',
         description: "Happy, Restless",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Havana+-+BLUE+STEEL.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Havana+BIG-+BLUE+STEEL.jpeg'
       },
       {
-        albumId: 2,
-        userId: 2,
+        albumId: 3,
+        userId: 3,
         title: 'Higher Self',
         description: "Hopeful, Laid Back",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Higher+Self+-+Xavy+Rusan.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Higher+Self+BIG-+Xavy+Rusan.jpeg'
       },
       {
-        albumId: 2,
-        userId: 2,
+        albumId: 4,
+        userId: 4,
         title: "I've Found My Way",
         description: "Happy, Hopeful",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: "https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_I've+Found+My+Way+-+Hallman.mp3",
+        previewImage: "https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_I've+Found+My+Way+BIG-+Hallman.jpeg"
       },
       {
-        albumId: 2,
-        userId: 2,
+        albumId: 5,
+        userId: 5,
         title: 'Sole Splende',
         description: "Happy, Laid Back",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Il+Sole+Splende+-+Sum+Wave.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Il+Sole+Splende+BIG+-+Sum+Wave.jpeg'
       },
       {
-        albumId: 2,
-        userId: 2,
+        albumId: 1,
+        userId: 1,
         title: 'Intimacy',
         description: "Hopeful, Laid Back",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Intimacy+-+Alex+Kehm.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Intimacy+BIG-+Alex+Kehm.jpeg'
       },
       {
         albumId: 2,
         userId: 2,
         title: 'Island In The Sky',
         description: "Happy, Dreamy",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: "https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Island+in+the+Sky+-+Giants'+Nest.mp3",
+        previewImage: "https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Island+in+the+Sky+BIG-+Giants'+Nest.jpeg"
       },
       {
         albumId: 2,
-        userId: 2,
+        userId: 3,
         title: 'Like the Ocean',
         description: "Hopeful, Dreamy",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Like+the+Ocean+-+The+Big+Let+Down.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Like+the+Ocean+-+The+Big+Let+Down.mp3'
       },
       {
         albumId: 2,
-        userId: 2,
+        userId: 4,
         title: 'Maharajah',
         description: "Happy Glamorous",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Maharajah+-+Ava+Low.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Maharajah+BIG-+Ava+Low.jpeg'
       },
       {
         albumId: 2,
-        userId: 2,
+        userId: 5,
         title: "Maybe We'll Meet Again",
         description: "Hopeful, Laid Back",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        url: "https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Maybe+We'll+Meet+Again+-+baegel.mp3",
+        previewImage: "https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Maybe+We'll+Meet+Again+BIG-+baegel.jpeg"
       },
       {
         albumId: 2,
-        userId: 2,
-        title: 'Song 8',
-        description: "Nice rap song",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
-      },
-      {
-        albumId: 2,
-        userId: 2,
-        title: 'Song 8',
-        description: "Nice rap song",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
-      },
-      {
-        albumId: 2,
-        userId: 2,
-        title: 'Song 8',
-        description: "Nice rap song",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
-      },
-      {
-        albumId: 2,
-        userId: 2,
-        title: 'Song 8',
-        description: "Nice rap song",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
-      },
-      {
-        albumId: 2,
-        userId: 2,
-        title: 'Song 8',
-        description: "Nice rap song",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
-      },
-      {
-        albumId: 2,
-        userId: 2,
-        title: 'Song 8',
-        description: "Nice rap song",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
-      },
-      {
-        albumId: 2,
-        userId: 2,
-        title: 'Song 8',
-        description: "Nice rap song",
-        url: 'www.songURLtesting.com',
-        previewImage: 'www.songIMGtesting.com'
+        userId: 1,
+        title: 'Not Blue',
+        description: "Hopeful, Sentimental",
+        url: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Not+Blue+-+Kylie+Dailey.mp3',
+        previewImage: 'https://soundcloud-files-hdt.s3.us-west-1.amazonaws.com/ES_Not+blue+BIG-+Kylie+Dailey.jpeg'
       },
     ], {});
   },
@@ -225,7 +177,7 @@ module.exports = {
   async down (queryInterface, Sequelize) {
     const Op = Sequelize.Op;
     await queryInterface.bulkDelete('Songs', {
-      title: { [Op.in]: ['Wonderful World', 'Hip-hop Song', 'Pop Song', 'Song 4', 'Song 5', 'Song 6', 'Song 7', 'Song 8'] }
+      id: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21] }
     }, {});
   }
 };
