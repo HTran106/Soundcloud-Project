476b209f989db128f0d6267e42da06d2acb08a5f
Huydu Tran <duutran87@icloud.com>
2022-07-06 15:10:02 -0700

    added modal files

---
+++ frontend/src/components/EditModal/EditForm.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch } from "react-redux";
+import './LoginForm.css'
+import { useHistory } from 'react-router-dom'
+
+function LoginForm() {
+  const dispatch = useDispatch();
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const history = useHistory()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
+  };
+
+  const loginGuestUser = e => {
+    e.preventDefault()
+    dispatch(sessionActions.loginGuest())
+    history.push('/')
+  }
+
+  return (
+    <>
+      <h1>Login to your SoundCloud account</h1>
+      <form onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+          <input
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+            placeholder="Username or Email"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            placeholder="Password"
+          />
+        <button className="login-button" type="submit">Login</button>
+      </form>
+      <form onSubmit={loginGuestUser}>
+        <p className='or'>----------------or----------------</p>
+        <button className='login-guest-button' type='submit'>Login as Guest</button>
+      </form>
+    </>
+  );
+}
+
+export default LoginForm;
