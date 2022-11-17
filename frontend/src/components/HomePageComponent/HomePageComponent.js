504ce6f1980173d1c823df81baf0a4074721f302
HTran106 <90004686+HTran106@users.noreply.github.com>
2022-11-17 14:01:53 -0800

    testing a link

--- frontend/src/components/HomePageComponent/HomePageComponent.js
+++ frontend/src/components/HomePageComponent/HomePageComponent.js
@@ -25,57 +25,58 @@ const HomePageComponent = () => {
     return (
         <div className="songs-container">
             <div className='song-sections'>
+                <a href='/songs/2'>testing link</a>
                 <h2>Top Trending Songs</h2>
                 <h4>Songs that will light the fire in you</h4>
-                    <ul className="songs-list">
-                        {trending?.map(song => (
-                            <div key={`${song.id}`} className='song-selections'>
-                                <button className='song-details-button' onClick={e => {
-                                    e.preventDefault()
-                                    history.push(`/songs/${song.id}`)
-                                    }}>
-                                    <img src={song.previewImage} alt='previewImage'></img>
-                                </button>
-                                <li key={song.id}>{song.title}</li>
-                                <p>🔥 🔥 🔥</p>
-                            </div>
-                        ))}
-                    </ul>
+                <ul className="songs-list">
+                    {trending?.map(song => (
+                        <div key={`${song.id}`} className='song-selections'>
+                            <button className='song-details-button' onClick={e => {
+                                e.preventDefault()
+                                history.push(`/songs/${song.id}`)
+                            }}>
+                                <img src={song.previewImage} alt='previewImage'></img>
+                            </button>
+                            <li key={song.id}>{song.title}</li>
+                            <p>🔥 🔥 🔥</p>
+                        </div>
+                    ))}
+                </ul>
             </div>
             <div className='song-sections'>
                 <h2>Top Trending Albums</h2>
                 <h4>Newest and Hottest Albums</h4>
-                    <ul className="songs-list">
-                        {newAndHot?.map(album => (
-                            <div key={`${album.id}`} className='song-selections'>
-                                <button className='song-details-button' onClick={e => {
-                                    e.preventDefault()
-                                    history.push(`/albums/${album.id}`)
-                                    }}>
-                                    <img src={album.previewImage} alt='previewImage'></img>
-                                </button>
-                                <li key={album.id}>{album.title}</li>
-                                <p>Listen to me NOW</p>
-                            </div>
-                        ))}
-                    </ul>
+                <ul className="songs-list">
+                    {newAndHot?.map(album => (
+                        <div key={`${album.id}`} className='song-selections'>
+                            <button className='song-details-button' onClick={e => {
+                                e.preventDefault()
+                                history.push(`/albums/${album.id}`)
+                            }}>
+                                <img src={album.previewImage} alt='previewImage'></img>
+                            </button>
+                            <li key={album.id}>{album.title}</li>
+                            <p>Listen to me NOW</p>
+                        </div>
+                    ))}
+                </ul>
             </div>
             <div className='all-song-sections'>
                 <h2>All Songs</h2>
                 <h4>All available songs</h4>
-                    <ul className="all-songs-list">
-                        {songs?.map(song => (
-                            <div key={`${song.id}`}className='all-song-selections'>
-                                <button className='all-song-details-button' onClick={e => {
-                                    e.preventDefault()
-                                    history.push(`/songs/${song.id}`)
-                                    }}>
-                                    <img src={song.previewImage} alt='previewImage'></img>
-                                </button>
-                                <li key={song.id}>{song.title}</li>
-                            </div>
-                        ))}
-                    </ul>
+                <ul className="all-songs-list">
+                    {songs?.map(song => (
+                        <div key={`${song.id}`} className='all-song-selections'>
+                            <button className='all-song-details-button' onClick={e => {
+                                e.preventDefault()
+                                history.push(`/songs/${song.id}`)
+                            }}>
+                                <img src={song.previewImage} alt='previewImage'></img>
+                            </button>
+                            <li key={song.id}>{song.title}</li>
+                        </div>
+                    ))}
+                </ul>
             </div>
         </div>
     )
